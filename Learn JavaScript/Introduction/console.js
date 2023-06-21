498585d83ad3a611de8748c1c62bbbc80cf0eac2
unofficialdxnny <ahmeddanial324@gmail.com>
2023-06-21 22:12:44 +0100

    console.js

---
+++ Learn JavaScript/Introduction/console.js
@@ -0,0 +1,8 @@
+// To print a string into the console
+// You may use "" or even ''
+console.log("This is a string") 
+console.log('This is also a string')
+
+// To print out a number into the console
+console.log(10)
+console.log(10.5)
