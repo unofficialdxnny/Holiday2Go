d17d5c19e1407cefb8be40a6e885ee8f64679b33
unofficialdxnny <ahmeddanial324@gmail.com>
2023-06-29 15:20:25 +0100

    adding some logic

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -3,3 +3,23 @@
   });
   
 
+// function form_submit() {
+//   alert('Generating your E-Ticket. Please Wait.')
+//   // return true
+// }
+
+
+function formValidation() {
+  var firstname = document.eticket.fname;
+  var lastname = document.eticket.lname;
+  var email = document.eticket.email;
+  var pnum = document.eticket.pnum;
+  var where = document.eticket.where;
+  var date = document.eticket.date;
+
+  if (firstname == "") {
+    alert('Enter Your First Name')
+  }
+
+
+}
