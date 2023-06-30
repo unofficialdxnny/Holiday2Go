cca64b29620d002857bfc62ff91fdb97e73d009a
unofficialdxnny <ahmeddanial324@gmail.com>
2023-06-30 13:39:12 +0100

    added logic for tickets

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -8,3 +8,14 @@ var destination = "This ticket is booked for a destination in " + localStorage.g
 var when = " On " + localStorage.getItem("Date")
 document.getElementById("destination").innerHTML = destination + when
 
+
+var img = document.getElementById("qrcode")
+
+
+if (localStorage.getItem("Destination") === 'London') {
+    img.src = "https://holiday2go.netlify.app/Assets/Images/1895e020b5a2f817d09bb14f8910afef.png"
+
+} else if (localStorage.getItem("Destination") === 'France') {
+    img.src = "https://holiday2go.netlify.app/Assets/Images/f8ae340c9da0cccc1dddd4026bf33e67.png"
+
+}
