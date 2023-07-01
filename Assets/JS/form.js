d8e3ca2d78d0f519d31c6327d066f5d4041e1a3b
unofficialdxnny <ahmeddanial324@gmail.com>
2023-07-01 01:00:55 +0100

    added  color coded qrcodes

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -13,11 +13,11 @@ var img = document.getElementById("qrcode")
 
 
 if (localStorage.getItem("Destination") === 'London') {
-    img.src = "https://holiday2go.netlify.app/Assets/Images/London.png"
+    img.src = "https://holiday2go.netlify.app/Assets/Images/3c7cda73e3964c9ba87d899116d538dc.png"
 
 } else if (localStorage.getItem("Destination") === 'France') {
-    img.src = "https://holiday2go.netlify.app/Assets/Images/France.png"
+    img.src = "https://holiday2go.netlify.app/Assets/Images/08f145f12b816afc02a7ee7673f0ec40.png"
 
 } else if (localStorage.getItem("Destination") === 'Belgium') {
-    img.src = "https://holiday2go.netlify.app/Assets/Images/Belgium.png"
+    img.src = "https://holiday2go.netlify.app/Assets/Images/ea1964466dfc193908b09c66370310d0.png"
 }
